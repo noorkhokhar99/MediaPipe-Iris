90f9e91249248b62b4215e52d77dbf4740c5cfde
Noorkhokhar <34125851+noorkhokhar99@users.noreply.github.com>
2022-11-29 12:25:49 +0500

    Add files via upload

--- demo.py
+++ demo.py
@@ -34,7 +34,7 @@ def get_args():
 
 
 def main():
-    # 引数 #####################################################################
+    # argument ################################################ ######################
     args = get_args()
 
     cap_device = args.device
@@ -45,12 +45,12 @@ def main():
     min_detection_confidence = args.min_detection_confidence
     min_tracking_confidence = args.min_tracking_confidence
 
-    # カメラ準備 ###############################################################
+    # Camera preparation ################################################ ################
     cap = cv.VideoCapture(cap_device)
     cap.set(cv.CAP_PROP_FRAME_WIDTH, cap_width)
     cap.set(cv.CAP_PROP_FRAME_HEIGHT, cap_height)
 
-    # モデルロード #############################################################
+    # Model load ################################################ ###############
     face_mesh = FaceMesh(
         max_num_faces,
         min_detection_confidence,
@@ -58,35 +58,35 @@ def main():
     )
     iris_detector = IrisLandmark()
 
-    # FPS計測モジュール ########################################################
+    # FPS Measurement Module ############################################### ##########
     cvFpsCalc = CvFpsCalc(buffer_len=10)
 
     while True:
         display_fps = cvFpsCalc.get()
 
-        # カメラキャプチャ #####################################################
+        # Camera capture ################################################ ######
         ret, image = cap.read()
         if not ret:
             break
-        image = cv.flip(image, 1)  # ミラー表示
+        image = cv.flip(image, 1) # mirror display
         debug_image = copy.deepcopy(image)
 
-        # 検出実施 #############################################################
-        # Face Mesh検出
+        # Detection ################################################ ###############
+        # Face Mesh detection
         face_results = face_mesh(image)
         for face_result in face_results:
-            # 目周辺のバウンディングボックス計算
+            # Calculate bounding box around eyes
             left_eye, right_eye = face_mesh.calc_around_eye_bbox(face_result)
 
-            # 虹彩検出
+            # Iris detection
             left_iris, right_iris = detect_iris(image, iris_detector, left_eye,
                                                 right_eye)
 
-            # 虹彩の外接円を計算
+            # Calculate the circumcircle of the iris
             left_center, left_radius = calc_min_enc_losingCircle(left_iris)
             right_center, right_radius = calc_min_enc_losingCircle(right_iris)
 
-            # デバッグ描画
+            # debug drawing
             debug_image = draw_debug_image(
                 debug_image,
                 left_iris,
@@ -100,12 +100,12 @@ def main():
         cv.putText(debug_image, "FPS:" + str(display_fps), (10, 30),
                    cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2, cv.LINE_AA)
 
-        # キー処理(ESC：終了) #################################################
+        # Key processing (ESC: end) ############################################ #######
         key = cv.waitKey(1)
         if key == 27:  # ESC
             break
 
-        # 画面反映 #############################################################
+        # Screen reflection ################################################# ###############
         cv.imshow('Iris(tflite) Demo', debug_image)
 
     cap.release()
@@ -118,30 +118,30 @@ def detect_iris(image, iris_detector, left_eye, right_eye):
     image_width, image_height = image.shape[1], image.shape[0]
     input_shape = iris_detector.get_input_shape()
 
-    # 左目
-    # 目の周辺の画像を切り抜き
+    # left eye
+    # Crop the image around the eyes
     left_eye_x1 = max(left_eye[0], 0)
     left_eye_y1 = max(left_eye[1], 0)
     left_eye_x2 = min(left_eye[2], image_width)
     left_eye_y2 = min(left_eye[3], image_height)
     left_eye_image = copy.deepcopy(image[left_eye_y1:left_eye_y2,
                                          left_eye_x1:left_eye_x2])
-    # 虹彩検出
+    # Iris detection
     eye_contour, iris = iris_detector(left_eye_image)
-    # 座標を相対座標から絶対座標に変換
+    # convert coordinates from relative to absolute
     left_iris = calc_iris_point(left_eye, eye_contour, iris, input_shape)
 
-    # 右目
-    # 目の周辺の画像を切り抜き
+    # right eye
+    # Crop the image around the eyes
     right_eye_x1 = max(right_eye[0], 0)
     right_eye_y1 = max(right_eye[1], 0)
     right_eye_x2 = min(right_eye[2], image_width)
     right_eye_y2 = min(right_eye[3], image_height)
     right_eye_image = copy.deepcopy(image[right_eye_y1:right_eye_y2,
                                           right_eye_x1:right_eye_x2])
-    # 虹彩検出
+    # Iris detection
     eye_contour, iris = iris_detector(right_eye_image)
-    # 座標を相対座標から絶対座標に変換
+    # convert coordinates from relative to absolute
     right_iris = calc_iris_point(right_eye, eye_contour, iris, input_shape)
 
     return left_iris, right_iris
@@ -155,7 +155,7 @@ def calc_iris_point(eye_bbox, eye_contour, iris, input_shape):
         point_y = int(iris[index * 3 + 1] *
                       ((eye_bbox[3] - eye_bbox[1]) / input_shape[1]))
         point_x += eye_bbox[0]
-        point_y += eye_bbox[1]
+        dot_y += eye_bbox[1]
 
         iris_list.append((point_x, point_y))
 
@@ -179,17 +179,17 @@ def draw_debug_image(
     right_center,
     right_radius,
 ):
-    # 虹彩：外接円
+    # Rainbow: circumscribed yen
     cv.circle(debug_image, left_center, left_radius, (0, 255, 0), 2)
     cv.circle(debug_image, right_center, right_radius, (0, 255, 0), 2)
 
-    # 虹彩：ランドマーク
+    # iris: landmark
     for point in left_iris:
         cv.circle(debug_image, (point[0], point[1]), 1, (0, 0, 255), 2)
     for point in right_iris:
         cv.circle(debug_image, (point[0], point[1]), 1, (0, 0, 255), 2)
 
-    # 虹彩：半径
+    # iridescence: radius
     cv.putText(debug_image, 'r:' + str(left_radius) + 'px',
                (left_center[0] + int(left_radius * 1.5),
                 left_center[1] + int(left_radius * 0.5)),
